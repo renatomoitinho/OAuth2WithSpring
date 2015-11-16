e7a1b8d3703fadfb82cbcfc91a393c4ce67e74e7
cin_redias <cin_redias@uolinc.com>
2015-11-16 14:32:39 -0200

    probe da aplicacao use http://localhost:9000/probe para status

--- data-rest-api/src/main/java/br/inf/audasi/api/ApplicationMain.java
+++ data-rest-api/src/main/java/br/inf/audasi/api/ApplicationMain.java
@@ -1,6 +1,7 @@
 package br.inf.audasi.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
@@ -9,6 +10,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
  * @author renatomoitinhodias@gmail.com
  */
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ApplicationMain {
 
     public class WebInitializer extends SpringBootServletInitializer {
